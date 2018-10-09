27ffb7f6c09ee2b264fcadb00db4e6fd8b4cce1e
Aayush Jha <aayushjha02@gmail.com>
2018-10-09 11:33:40 -0400

    Finished Pset3

--- README.md
+++ README.md
@@ -1,52 +1,3 @@
-# Problem Set 3
-
-Problem Set 3 challenges your ability to use conditional and iterative control structures, as well as relational, logical, and other operators to make decisions and produce outputs.
-
-## Getting Started
-
-To get started, you'll need to create a [GitHub](https://github.com/) repository to store your Problem Set 3 code. After cloning my skeleton repository, you'll need to setup a remote to push your code to your repository instead of mine. Steps to accomplish this are outlined below.
-
-### Setup
-
-01. Login to your [GitHub](https://github.com/) account and create a new repository named ```problem-set-3```.
-02. In GitBash, navigate to your ```APCSA``` folder.
-03. Clone my skeleton repository from [GitHub](https://github.com/). This will make a copy of my repository and store it locally.
-```
-   >>> git clone git@github.com:rwilson-ucvts/problem-set-3-skeleton.git
-```
-04. The cloning process will have created a folder named ```problem-set-3-skeleton```. Rename this folder to ```pset3```.
-```
-   >>> mv problem-set-3-skeleton pset3
-```
-05. Change directories to get into your ```pset3``` folder.
-```
-   >>> cd pset3
-```
-06. Originally, the remote will be pointing at my repository. We need to overwrite this.
-```
-   >>> git remote rename origin upstream
-```
-07. Lastly, we need to add a new remote that points at the repository you created earlier. Make sure you replace ```YOUR-USERNAME``` with your actual username.
-```
-   >>> git remote add pset3 git@github.com:YOUR-USERNAME/problem-set-3.git
-```
-08. Launch Eclipse and set the ```Workspace``` to the ```APCSA``` folder you created on your ```Desktop```. Make sure you're using your ```Workspace```, as others' will be similarly named.
-09. From within the ```Package Explorer``` (the left-most panel), right-click and select ```Import...```.
-10. Select ```Git > Projects from Git```, and click ```Next >```.
-11. Select ```Existing local repository``` and click ```Next >```.
-12. Click the ```Add...``` button, and then the ```Browse...``` button.
-13. Navigate to the ```APCSA``` folder on your ```Desktop```, click the ```pset3``` project folder, and click ```Open```.
-14. Select the checkbox next to your project and click ```Finish```.
-15. Now that we've imported the Git project, we can click ```Next >```, ```Next >```, and ```Finish``` once more.
-
-You should now see a ```Project``` in the ```Package Explorer``` in Eclipse.
-
-16. Expand the ```Project``` folder. You should see the ```JRE System Library``` folder, as well as the ```src``` folder.
-17. Expand the src folder. You should see a single source file named ```ProblemSet3.java```. Double-click to open it.
-
-If you see the following starter code, then you've correctly cloned and setup your project.
-
-![problem set 3 skeleton code screenshot](https://www.ucvts.tec.nj.us/cms/lib/NJ03001805/Centricity/domain/760/apcsa-images/pset3-skeleton.png)
 
 ## Exercises
 
@@ -110,7 +61,7 @@ This one's pretty simple. Given three integer values (i.e., the ```a```, ```b```
 
 ```
 loneSum(1, 2, 3) should yield 6.
-loneSum(3, 2, 2) should yield 2.
+loneSum(3, 2, 3) should yield 2.
 loneSum(3, 3, 3) should yield 0.
 ```
 
@@ -126,7 +77,7 @@ luckySum(1, 13, 3) should yield 1.
 
 ### Exercise 8 (```factorialWithFor```)
 
-This one couldn't get any more straightforward. Given some integer value (i.e., the parameter ```n```), comput its factorial. The factorial of an integer is that integer multiplied by all positive integers less than it (i.e., 5! = 5 * 4 * 3 * 2 * 1 = 120). Use a ```for``` loop to solve this.
+This one couldn't get any more straightforward. Given some integer value (i.e., the parameter ```n```), compute its factorial. The factorial of an integer is that integer multiplied by all positive integers less than it (i.e., 5! = 5 * 4 * 3 * 2 * 1 = 120). Use a ```for``` loop to solve this.
 
 ```
 factorialWithFor(3) should yield 3! = 6.
--- src/ProblemSet3.java
+++ src/ProblemSet3.java
@@ -12,7 +12,8 @@
  * A parameter is data that is provided to the method. It helps the method do its job. It's passed in
  * through the parentheses, and you can reference these variables by name as if they're your own.
  */
-
+import java.util.Scanner;
+import static java.lang.Math.sqrt;
 public class ProblemSet3 {
 	
 	/**
@@ -20,46 +21,176 @@ public class ProblemSet3 {
 	 */
 	
 	public static void main(String[] args) {
-			
+		Scanner in = new Scanner(System.in);
+
+		ProblemSet3 ps3 = new ProblemSet3();
+		//ps3.dateFashion(6, 3);
+		//ps3.fizzString("boolin");
+		//ps3.squirrelPlay(94, true);
+		//ps3.fizzStringAgain(18);
+		//ps3.makeBricks(3, 2, 10);
+		//ps3.loneSum(3, 3, 3);
+		//ps3.factorialWithFor(3);
+		//ps3.factorialWithWhile(3);
+		ps3.isPrime(1);
+		
 	}
 		
 	public void dateFashion(int you, int date) {
-		
+		if (you <= 2 || date <= 2) {
+			System.out.println("You will not get a table");
+		}
+		else if (you >= 8 || date >= 8) {
+			System.out.println("You will get a table");
+		}
+		else {
+			System.out.println("You will maybe get a table");
+		}
 	}
 	
 	public void fizzString(String str) {
-		
+		char init = str.charAt(0);
+		int length = str.length() - 1;
+		char last = str.charAt(length);
+		if (init == 'f' && last == 'b') {
+			System.out.println("FIZZBUZZ");
+		}
+		else if (init == 'f') {
+			System.out.println("FIZZ");
+		}
+		else if (last == 'b') {
+			System.out.println("BUZZ");
+		}
+		else {
+			System.out.println(str);
+		}
 	}
 		
 	public void squirrelPlay(int temp, boolean isSummer) {
-		
+		if (isSummer == true && temp <= 100) {
+			System.out.println("YES");
+		}
+		else if (isSummer == false && temp > 90 ) {
+			System.out.println("NO");
+		}
+		else if (temp < 60) {
+			System.out.println("YES");
+		}
 	}
 	
 	public void fizzStringAgain(int n) {
-		
+		if (n % 5 == 0 && n % 3 == 0) {
+			System.out.println("FIZZBUZZ!");
+		}
+		else if (n % 3 == 0) {
+			System.out.println("FIZZ!");
+		}
+		else if (n % 5 == 0) {
+			System.out.println("BUZZ!");
+		}
+		else {
+			System.out.println(n + "!");
+		}
 	}
 	
 	public void makeBricks(int small, int big, int goal) {
+		int bigCount = goal / 5;
+		int smallCount = goal % 5;
+		if (small >= goal) {
+			System.out.println("Can be built");
+		}
+		else if(goal - big*5 > 0) {
+			if ((goal - big*5) <= small) {
+			System.out.println("Can be built");
+			}
+		}
+		else if (bigCount > big) {
+			System.out.println("Cannot be built");
+		}
+		else if (smallCount > small) {
+			System.out.println("Cannot be built");
+		}
 		
+		else if (bigCount <= big && smallCount <= small) {
+			System.out.println("Can be built");
+		}
 	}
 	
 	public void loneSum(int a, int b, int c) {
-		
+		int sum;
+		if (a != b && b != c && a != c) {
+			sum = a + b + c;
+			System.out.println(sum);
+		}
+		else if (a == b && b != c && a != c) {
+			sum = c;
+			System.out.println(sum);
+		}
+		else if (a == b && b == c && a == c) {
+			sum = 0;
+			System.out.println(sum);
+		}
+		else if (a != b && b != c && a == c) {
+			sum = b;
+			System.out.println(sum);
+		}
 	}
 	
 	public void luckySum(int a, int b, int c) {
-		
+		int sum;
+		if (a == 13)
+		{
+			sum = 0;
+		}
+		else if (b == 13) {
+			sum = a;
+		}
+		else if (c == 13) {
+			sum = a + b;
+		}
+		else {
+			sum = a + b + c;
+		}
+		System.out.println("The sum is:" + sum);
 	}
 	
 	public void factorialWithFor(int n) {
-		
+		int fact = n;
+		int count = n;
+		for (int i = 1; i < n; i++) {
+			fact = fact * (count-1);
+			count--;
+		}
+		System.out.println(fact + ".");
 	}
 	
 	public void factorialWithWhile(int n) {
-		
+		int fact = n;
+		int count = n;
+		int i = 1;
+		while (i < (n-1)) {
+			fact = fact * (count-1);
+			i++;
+		}
+		System.out.println(fact + ".");
 	}
 	
 	public void isPrime(int n) {
-		
+		boolean mark = false;
+		Double root = Math.sqrt(n);
+		for (double i = 2; i <= root; i++) {
+			double check = n % i;
+			if (check == 0) {
+				System.out.println("NOT PRIME");
+				mark = true;
+				break;
+			}
+		}
+		if (n == 1) {
+			System.out.println("NOT PRIME");
+		}
+		else if (mark == false) {
+			System.out.println("PRIME");
+		}
 	}
 }
